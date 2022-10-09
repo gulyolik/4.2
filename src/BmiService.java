c3abe769c9c8958e3a3e0387f2a2093d4c7d43bd
gulyolik <olia.guliaeva@yandex.ru>
2022-10-09 18:45:56 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,5 @@
+public class BmiService {
+    public int calculate (int weight, float height) {
+        int result = (int) (weight / (height * height));
+    return result;}
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+    BmiService service = new BmiService();
+    service.calculate (90, (float) 1.63);
+    int bmi = service.calculate(90, (float) 1.63);
+    System.out.println("Индекс массы тела человека равен " + bmi);
+    }
+}
